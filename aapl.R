231383d1c76413d0fa2490079fedcda31fbedecb
root <root@c11node8.(none)>
2014-03-23 04:43:34 -0400

    aapl.R

--- aapl.R
+++ aapl.R
@@ -29,6 +29,8 @@ plot(aapl[,5], xlab = "DAY", ylab = "STOCK VALUE ($)", type = "l", col="blue")
 # open value
 lines(aapl[,2], type = "l", col="red")
 
+legend("topleft", c("Open", "Close"))
+
 #plot(aapl[c(1:40), 1], aapl[c(1:40),5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue")
 #plot(aapl[c(1:40),5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue")
 #plot(aapl[,1], aapl[,5], type = "l", col="blue")
