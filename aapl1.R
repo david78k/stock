f0e33a5abb1233938736f6734bdd7ccb4a0a6d09
root <root@c11node8.(none)>
2014-03-23 16:32:01 -0400

    aapl1.R

--- aapl1.R
+++ aapl1.R
@@ -23,7 +23,6 @@ print(googdata)
 #box()
 
 # close value
-#plot(aapl[,5], type = "l", col="blue")
 #plot(aapl[,1], aapl[,5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue")
 plot(aapl[,5], xlab = "DAY", ylab = "STOCK VALUE ($)", type = "l", col="blue")
 
@@ -31,7 +30,8 @@ plot(aapl[,5], xlab = "DAY", ylab = "STOCK VALUE ($)", type = "l", col="blue")
 lines(aapl[,2], type = "l", col="red")
 
 #legend("topleft", names(aapldata)[c(2,5)], fill = heat.colors(2))
-legend("topleft", names(aapldata)[c(2,5)], lty = 1, col = c('red', 'blue'))
+legend("topleft", names(aapldata)[c(2,5)], lty = 1:2, col = c('red', 'blue'))
+#legend("topleft", names(aapldata)[c(2,5)], lty = 1, col = c('red', 'blue'))
 
 #matplot(aapl[,1], aapl[,5], type = "l", col="red")
 #plot(aapl[c(1:40), 1], aapl[c(1:40),5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue")
