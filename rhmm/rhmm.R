c8d42ce47b92727484f65412e79f5e81ddc8264b
root <root@c11node8.(none)>
2014-03-24 15:41:26 -0400

    all

--- rhmm/rhmm.R
+++ rhmm/rhmm.R
@@ -10,3 +10,12 @@ hm_model <- HMMFit(obs = TWII_Train, nStates = 5)
 
 # Viterbi Algorithm
 VitPath <- viterbi (hm_model, TWII_Train)
+
+# scatter plot
+TWII_Predict <- cbind(TWII_Subset$TWII.Close, VitPath$states)
+chartSeries(TWII_Predict[,1])
+addTA(TWII_Predict[TWII_Predict[,2]==1,1],on=1,type="p",col=5,pch=25)
+addTA(TWII_Predict[TWII_Predict[,2]==2,1],on=1,type="p",col=6,pch=24)
+addTA(TWII_Predict[TWII_Predict[,2]==3,1],on=1,type="p",col=7,pch=23)
+addTA(TWII_Predict[TWII_Predict[,2]==4,1],on=1,type="p",col=8,pch=22)
+addTA(TWII_Predict[TWII_Predict[,2]==5,1],on=1,type="p",col=10,pch=21)
