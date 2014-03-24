5b2e72ab535f8999153d8515852c1f89e1009c6e
root <root@c11node8.(none)>
2014-03-24 16:43:12 -0400

    AAPL.R

--- rhmm/AAPL.R
+++ rhmm/AAPL.R
@@ -2,22 +2,22 @@ library(quantmod)
 
 getSymbols("AAPL")
 chartSeries(AAPL)
- <- window(AAPL, start = as.Date("2012-01-01"))
- <- cbind($AAPL.Close - $AAPL.Open, $AAPL.Volume)
+AAPL_Subset <- window(AAPL, start = as.Date("2012-01-01"))
+AAPL_Train <- cbind(AAPL_Subset$AAPL.Close - AAPL_Subset$AAPL.Open, AAPL_Subset$AAPL.Volume)
 
 library(RHmm)
 # Baum-Welch Algorithm
-hm_model <- HMMFit(obs = , nStates = 5)
+hm_model <- HMMFit(obs = AAPL_Train, nStates = 5)
 
 # Viterbi Algorithm
-VitPath <- viterbi (hm_model, )
+VitPath <- viterbi (hm_model, AAPL_Train)
 
 # scatter plot
- <- cbind($AAPL.Close, VitPath)
-chartSeries([,1])
-addTA([[,2]==1,1],on=1,type="p",col=5,pch=25)
-addTA([[,2]==2,1],on=1,type="p",col=6,pch=24)
-addTA([[,2]==3,1],on=1,type="p",col=7,pch=23)
-addTA([[,2]==4,1],on=1,type="p",col=8,pch=22)
-addTA([[,2]==5,1],on=1,type="p",col=10,pch=21)
+AAPL_Predict <- cbind(AAPL_Subset$AAPL.Close, VitPath)
+chartSeries(AAPL_Predict[,1])
+addTA(AAPL_Predict[AAPL_Predict[,2]==1,1],on=1,type="p",col=5,pch=25)
+addTA(AAPL_Predict[AAPL_Predict[,2]==2,1],on=1,type="p",col=6,pch=24)
+addTA(AAPL_Predict[AAPL_Predict[,2]==3,1],on=1,type="p",col=7,pch=23)
+addTA(AAPL_Predict[AAPL_Predict[,2]==4,1],on=1,type="p",col=8,pch=22)
+addTA(AAPL_Predict[AAPL_Predict[,2]==5,1],on=1,type="p",col=10,pch=21)
 
