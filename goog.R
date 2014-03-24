f608190807f4d361e036a14c6b12b7d0ee7d36e7
root <root@c11node8.(none)>
2014-03-23 23:15:26 -0400

    goog.R

--- goog.R
+++ goog.R
@@ -24,11 +24,11 @@ write.table(goog, file = "goog.tab")
 
 # open value
 #plot(aapl[,1], aapl[,5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue")
-plot(as.numeric(goog[,2]), xlab = "DAY", ylab = "STOCK VALUE ($)", col="blue")
+plot(as.numeric(goog[,2]), xlab = "DAY", ylab = "STOCK VALUE ($)", lty = 1, col="blue")
 #plot(goog[,2], xlab = "DAY", ylab = "STOCK VALUE ($)", type = "l", col="blue")
 
 # close value
-lines(goog[,5], type = "l", col="red")
+lines(goog[,5], lty = 2, col="red")
 
 #legend("topleft", names(data)[c(2,5)], fill = heat.colors(2))
 legend("topleft", names(data)[c(2,5)], lty = 1:2, col = c('red', 'blue'))
