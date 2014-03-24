83f7b13afe7d3a9df911454a83bc12ba0be5269c
root <root@c11node8.(none)>
2014-03-24 15:58:27 -0400

    all

--- rhmm/rhmm.R
+++ rhmm/rhmm.R
@@ -3,7 +3,7 @@ library(quantmod)
 getSymbols("AAPL")
 chartSeries(AAPL)
 TWII_Subset <- window(AAPL, start = as.Date("2012-01-01"))
-TWII_Train <- cbind(TWII_Subset$TWII.Close - TWII_Subset$AAPL.Open, TWII_Subset$AAPL.Volume)
+TWII_Train <- cbind(TWII_Subset$AAPL.Close - TWII_Subset$AAPL.Open, TWII_Subset$AAPL.Volume)
 
 library(RHmm)
 # Baum-Welch Algorithm
@@ -13,7 +13,7 @@ hm_model <- HMMFit(obs = TWII_Train, nStates = 5)
 VitPath <- viterbi (hm_model, TWII_Train)
 
 # scatter plot
-TWII_Predict <- cbind(TWII_Subset$TWII.Close, VitPath$states)
+TWII_Predict <- cbind(TWII_Subset$AAPL.Close, VitPath$states)
 chartSeries(TWII_Predict[,1])
 addTA(TWII_Predict[TWII_Predict[,2]==1,1],on=1,type="p",col=5,pch=25)
 addTA(TWII_Predict[TWII_Predict[,2]==2,1],on=1,type="p",col=6,pch=24)
