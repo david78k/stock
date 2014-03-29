ad379a85fece7bc572b1d036a707096c1e338fa3
root <root@c11node8.(none)>
2014-03-29 01:04:07 -0400

    GOOG.R

--- rhmm/GOOG.R
+++ rhmm/GOOG.R
@@ -19,7 +19,7 @@ VitPath <- viterbi (hm_model, GOOG_Train)
 postscript('GOOG.eps')
 GOOG_Predict <- cbind(GOOG_Subset$GOOG.Close, VitPath$states)
 #chartSeries(GOOG_Predict[,1], #theme="white.mono", 
-chartSeries(GOOG_Predict[,1], #layout = layout(matrix(c(2, 0), 1, 2, byrow = TRUE), #respect = TRUE), #theme="white.mono", 
+chartSeries(GOOG_Predict[,1], layout = layout(matrix(c(2, 0), 1, 2, byrow = TRUE), #respect = TRUE), #theme="white.mono", 
 TA="addTA(GOOG_Predict[GOOG_Predict[,2]==1,1],on=1, col=5,pch=25);
 addTA(GOOG_Predict[GOOG_Predict[,2]==2,1],on=1,type='p',col=6,pch=24);
 addTA(GOOG_Predict[GOOG_Predict[,2]==3,1],on=1,type='p',col=7,pch=23);
