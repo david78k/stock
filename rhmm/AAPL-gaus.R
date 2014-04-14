e5e12b13fee7c2fe4097f0f5eb5828bc5f32563d
root <root@c11node8.(none)>
2014-04-14 01:28:30 -0400

    AAPL-gaus.R

--- rhmm/AAPL-gaus.R
+++ rhmm/AAPL-gaus.R
@@ -41,7 +41,8 @@ AAPL_Predict <- cbind(trainset$AAPL.Close, VitPath$states)
 #print(matrix(unlist(hm_model$HMM$distribution$proportion[1,])))
 
 vec <- 1:length(testset)
-testset$Pred <- vec
+testset$Pred <- apply(testset, 1, vec)
+#testset$Pred <- vec
 #testset["Pred"] <- vec
 #testset["Pred"] <- NA
 #testopen <- testset$AAPL.Open
