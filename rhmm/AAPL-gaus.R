e2776058297362be0b98641da3c73ac776f7243d
root <root@c11node8.(none)>
2014-04-14 14:14:16 -0400

    AAPL-gaus.R

--- rhmm/AAPL-gaus.R
+++ rhmm/AAPL-gaus.R
@@ -47,9 +47,9 @@ testset <- cbind(testset, Pred = 0)
 
 #for (i in 1: length(testopen)) {
 #for (i in 1: length(testset)) {
-#for (i in 1: length(testset) - 1) {
 #for (i in 1: 3) {
-for (i in 1: 252) {
+#for (i in 1: 251) {
+for (i in 1: length(testset) - 1) {
 	testrow <- testset[i, ]
 	print(testrow)
 	testopen <- testset$AAPL.Open[i, ]
