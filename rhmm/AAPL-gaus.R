1f87d904acef0c1fafca3f81178ba9b24461ef60
root <root@c11node8.(none)>
2014-04-13 23:40:02 -0400

    AAPL-gaus.R

--- rhmm/AAPL-gaus.R
+++ rhmm/AAPL-gaus.R
@@ -42,7 +42,7 @@ print(unlist(hm_model$HMM$distribution$mean))
 #print(matrix(unlist(hm_model$HMM$distribution$proportion[1,])))
 
 testopen <- testset$AAPL.Open
-for (i in 1: length(testset)) {
+for (i in 0: length(testset) - 1) {
 	testrow <- testopen[i, ]
 	print(testrow)
 	#print(testset$AAPL.Open[i, ])
