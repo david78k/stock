c0bff142c8fdf2a801094420703f9e38b033dce5
root <root@c11node8.(none)>
2014-04-26 10:36:34 -0400

    GOOG.R

--- rhmm/GOOG.R
+++ rhmm/GOOG.R
@@ -8,7 +8,7 @@ getSymbols("GOOG", src = "google")
 #getSymbols("GOOG")
 chartSeries(GOOG, theme="white")
 #trainset <- window(GOOG, start = as.Date("2010-09-07"), end = as.Date("2013-04-01"))
-trainset <- window(GOOG, start = as.Date("2010-09-07"), end = as.Date("2013-04-01"), na.rm)
+trainset <- window(GOOG, start = as.Date("2010-09-07"), end = as.Date("2013-04-01"), na.rm=TRUE)
 print(trainset)
 
 #GOOG_Subset <- window(GOOG, start = as.Date("2010-09-07"), end = as.Date("2013-04-01"))
